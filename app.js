6de23a7f4f9abe8af56b4323ba2c1aff14898af1
vamakin <vamakin@gmail.com>
2015-11-23 10:07:01 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
